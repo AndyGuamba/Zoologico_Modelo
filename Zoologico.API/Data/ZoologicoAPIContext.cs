865c5b352d98e4e54300df272caa4cf261a92c98
ANDY JOEL GUAMBA ROMAN <ajguambar@utn.edu.ec>
2025-12-02 21:21:18 -0500

    Agregar archivos de proyecto.

---
+++ Zoologico.API/Data/ZoologicoAPIContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Zoologico_Modelo;
+
+    public class ZoologicoAPIContext : DbContext
+    {
+        public ZoologicoAPIContext (DbContextOptions<ZoologicoAPIContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Zoologico_Modelo.Especie> Especie { get; set; } = default!;
+
+public DbSet<Zoologico_Modelo.Raza> Raza { get; set; } = default!;
+
+public DbSet<Zoologico_Modelo.Animal> Animal { get; set; } = default!;
+    }
---
+++ Zoologico_Modelo/Animal.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zoologico_Modelo
+{
+    public class Animal
+    {
+        [Key] public int Id {  get; set; }
+        public string Name { get; set; }
+
+        public int EspecieId {  get; set; }
+        public int RazaId {  get; set; }
+        public Raza? IdRaza { get; set; }
+        public Especie? IdEspecie { get; set; }
+
+
+    }
+}
